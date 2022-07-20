d8f2cc024628cc484b656247f46c3c93d34fc7bb
juliospelucio <juliopeluciofox@hotmail.com>
2022-07-19 21:16:20 -0300

    feat - MailView component

--- src/components/MailTable.vue
+++ src/components/MailTable.vue
@@ -2,7 +2,7 @@
     <table class="mail-table">
         <tbody>
             <tr v-for="email in unarquivedEmails" :key="email.id" :class="['clickable', email.read ? 'read' : '']"
-                @click="readEmail(email)">
+                @click="openEmail(email)">
                 <td>
                     <input type="checkbox">
                 </td>
@@ -16,20 +16,25 @@
                 <td><button @click="archiveEmail(email)">Archive</button></td>
             </tr>
         </tbody>
-
     </table>
+    <MailView v-if="openedEmail" :email="openedEmail" />
 </template>
 
 <script>
 import { format } from 'date-fns';
 import axios from 'axios';
+import MailView from '@/components/MailView.vue';
 export default {
     async setup() {
         let { data: emails } = await axios.get('http://localhost:3000/emails');
         return {
             format,
-            emails
+            emails,
+            openedEmail: null
         }
+    },
+    components: {
+        MailView
     },
     computed: {
         sortedEmails() {
@@ -42,9 +47,10 @@ export default {
         }
     },
     methods: {
-        readEmail(email) {
+        openEmail(email) {
             email.read = true
             this.updateEmail(email)
+            this.openedEmail = email
         },
         archiveEmail(email) {
             email.archived = true
